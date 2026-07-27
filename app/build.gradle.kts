05168b066b8c239ff7bad44fb2ebf9d4c9b05f04
mini <nimuyman@gmail.com>
2026-07-27 20:09:18 +0900

    chore: 리뷰 토큰 및 버전 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.veryshinnam.myapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.7.0"
+        versionCode = 17
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/veryshinnam/myapp/core/session/ReviewToken.kt
+++ app/src/main/java/com/veryshinnam/myapp/core/session/ReviewToken.kt
@@ -3,7 +3,7 @@ package com.veryshinnam.myapp.core.session
 // 심사 기간용 토큰
 object ReviewToken {
     const val REVIEW_ACCESS_TOKEN =
-        "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inl1XzAyMTZAbmF2ZXIuY29tIiwiaWF0IjoxNzc4MDQ2MDE0LCJleHAiOjE3ODA2MzgwMTR9.1gsHpCZGToGu2WrL2S1-tXdH_z9dmioEDFHqTndua3M"
+        "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inl1XzAyMTZAbmF2ZXIuY29tIiwiaWF0IjoxNzg1MTQ3MjE2LCJleHAiOjE3ODc3MzkyMTZ9.YeNZyCsHxIvfRoZbqK8skfllHwR8MHJ8ElG5roA--Ug"
 
-    const val REVIEW_EXPIRE_AT = "2026-05-08T00:00:00"
+    const val REVIEW_EXPIRE_AT = "2026-07-28T23:59:00"
 }
